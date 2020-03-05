7ee58d951dc9d51fe511b3cbb9b332c9fd58ce6a
timdotd <tim@duckett.de>
2020-03-05 13:19:09 +0100

    Initial Commit

---
+++ ComposableMVP/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ComposableMVP
+//
+//  Created by Tim on 05.03.20.
+//  Copyright © 2020 duckett.de. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
